cf9ef6f75470a593dc5e8a70732cdfaa54de2a14
butaneko <butaneko@buta.neko>
2021-03-22 22:14:22 +0900

    2021年3月22日-1

--- config/settings.py
+++ config/settings.py
@@ -114,9 +114,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = 'staticfiles'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = 'staticfiles'
+PROJECT_NAME = os.path.basename(BASE_DIR)
+STATIC_ROOT = 'var/www/{}/static'.format(PROJECT_NAME)
 
 try:
     from .local_settings import *
