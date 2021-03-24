480bd0dc6ae3b0bbdf9b9d82693184cd0507a3c1
butaneko <butaneko@buta.neko>
2021-03-24 12:56:04 +0900

    3

--- config/settings.py
+++ config/settings.py
@@ -4,9 +4,10 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# DEBUG = False
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
+# ブラウザでエラー確認の時だけ
 DEBUG = True
 SECRET_KEY = 'hao@_p9(=+6x#z)(8%6q2c!ew0xm2h4@o-7o)m@4_5zscrl$j#'
 
