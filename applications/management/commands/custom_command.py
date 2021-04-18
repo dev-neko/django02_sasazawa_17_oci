aa626982af6adeb358d2a41aedea2b2f77d9ee04
butaneko <butaneko@buta.neko>
2021-04-18 12:01:55 +0900

    commited

--- applications/management/commands/custom_command.py
+++ applications/management/commands/custom_command.py
@@ -225,24 +225,26 @@ def get_db_all_data():
 # selenium設定
 def boot_selenium():
 	chrome_options=webdriver.ChromeOptions()
+	"""
 	# アダプタエラー、自動テスト…、を非表示
 	chrome_options.add_experimental_option("excludeSwitches",['enable-automation',
 																														'enable-logging'])
-	# chrome_options.add_experimental_option("debuggerAddress","127.0.0.1:9222")
 	chrome_options.add_argument('--headless')  #ヘッドレスモード
 	chrome_options.add_argument('--incognito')  #シークレットモード
 	chrome_options.add_argument('--disable-gpu')
 	chrome_options.add_argument('--disable-desktop-notifications')
 	chrome_options.add_argument("--disable-extensions")
-	# chrome_options.add_argument('--disable-dev-shm-usage') #/dev/shmを使わないように指定
+	chrome_options.add_argument('--disable-dev-shm-usage') #/dev/shmを使わないように指定
 	chrome_options.add_argument('--disable-application-cache')
 	chrome_options.add_argument('--no-sandbox')
 	chrome_options.add_argument('--single-process')
 	chrome_options.add_argument('--ignore-certificate-errors')
 	chrome_options.add_argument('--user-agent=aheahe')
 	chrome_options.add_argument('--blink-settings=imagesEnabled=false') #画像を非表示
-	# chrome_options.add_argument('--remote-debugging-port=9222') #
 	chrome_options.page_load_strategy='none' #
+	"""
+	# chrome_options.add_argument('--remote-debugging-port=9222') #
+	# chrome_options.add_experimental_option("debuggerAddress","127.0.0.1:9222")
 	driver=webdriver.Chrome(options=chrome_options)
 	# driver.maximize_window()
 	return driver
