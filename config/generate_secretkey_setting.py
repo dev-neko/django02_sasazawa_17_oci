38dec069f32040a7843917f2f9e58c534551aad0
dev-neko <dev-neko@dev.neko>
2022-06-24 20:24:20 +0900

    commit

--- config/generate_secretkey_setting.py
+++ config/generate_secretkey_setting.py
@@ -1,8 +1,3 @@
-'''
-https://qiita.com/haessal/items/abaef7ee4fdbd3b218f5
-ここの記事を参考にして、プロジェクトをクローンするごとに SECRET_KEY を生成する形にした
-'''
-
 from django.core.management.utils import get_random_secret_key
 
 secret_key = get_random_secret_key()
