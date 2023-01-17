0b2d247f0d2914b9cdca6a9e72a8c38a1c7a9a35
dev-neko <dev-neko@dev.neko>
2023-01-17 23:22:46 +0900

    2023年1月17日

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import os
 import subprocess
 
 
+# requirements.txtから一括インストール
 cmd="pip install -r requirements.txt"
 subprocess.run(cmd.split(' '))
 
 cmd="python manage.py collectstatic --noinput"
 subprocess.run(cmd.split(' '))
 
-# CA証明書をDLする
+# CA証明書をDL
 # cmd=os.getenv("DOWNLOAD_CA_CERT")
 # subprocess.run(cmd.split(' '))
 
